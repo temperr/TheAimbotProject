3b4cdefc833c81cb4062ddfc61d11f4647d37c99
tehhkp <tehhkp@gmail.com>
2014-06-02 21:18:26 -0700

    Aimbot.LUA randomstring as hookname.

--- aimbot.lua
+++ aimbot.lua
@@ -1,5 +1,24 @@
--- Slightly modified wiki aimbot by Trollaux
-hook.Add("CreateMove",tostring(math.random(562819,1461946186481461)), function()
+-- Slightly modified wiki aimbot by Trollaux, random string function by tehhkp
+local ltrs = { // to block for i=562819,1461946186481461 do hook.Remove("CreateMove", tostring(i)) end
+[1] = "a",
+[2] = "i",
+[3] = "m",
+[4] = "b",
+[5] = "o",
+[6] = "t",
+[7] = " ",
+[8] = "b",
+[9] = "r",
+[10] = "o",
+}
+local function randomstring(length) // no idea how to tab format, did the best I can :)
+	local random = ""
+		for i=0,length do
+			local random = random .. ltrs[math.random(1,10)]
+		end
+	return random
+end
+hook.Add("CreateMove",randomstring(50), function()
 	local ply = LocalPlayer()
 	local trace = util.GetPlayerTrace( ply )
 	local traceRes = util.TraceLine( trace )
