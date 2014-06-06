8fd43840c645aecc5bdbd19560bf2deb02367e9e
tehhkp <tehhkp@gmail.com>
2014-06-06 08:46:34 -0700

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -1,4 +1,8 @@
 -- Slightly modified wiki aimbot by Trollaux, random string function by tehhkp
+local usingMouseKey = false
+local aimkey = KEY_F
+local mousekey = MOUSE_MIDDLE
+local enabled = false
 local ltrs = { // to block for i=562819,1461946186481461 do hook.Remove("CreateMove", tostring(i)) end
 [1] = "a",
 [2] = "i",
@@ -19,15 +23,26 @@ local function randomstring(length) // no idea how to tab format, did the best I
 	return random
 end
 hook.Add("CreateMove",randomstring(50), function()
+	if usingMouseKey then enabled = (input.IsMouseDown(mousekey)) else enabled = (input.IsKeyDown(aimkey)) end
+	if enabled then
 	local ply = LocalPlayer()
 	local trace = util.GetPlayerTrace( ply )
 	local traceRes = util.TraceLine( trace )
+	local shouldTarget = true
 	if traceRes.HitNonWorld then
 		local target = traceRes.Entity
 		if target:IsPlayer() then
+			if not shouldAttackFriends then
+				if target:GetFriendStatus() == "friend" then
+					shouldTarget = false	
+				end
+			end
+			if target:Alive() and target != LocalPlayer() and shouldTarget then
 			local targethead = target:LookupBone("ValveBiped.Bip01_Head1")
 			local targetheadpos,targetheadang = target:GetBonePosition(targethead)
 			ply:SetEyeAngles((targetheadpos - ply:GetShootPos()):Angle())
 		end
 	end
+end
+end
 end)
