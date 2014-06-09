20da2809f379a52ef4e0df58273ad4bb21e9e604
tehhkp <tehhkp@gmail.com>
2014-06-08 20:34:00 -0700

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -1,27 +1,65 @@
--- Slightly modified wiki aimbot by Trollaux, random string function by tehhkp
+--[[
+    Credits:
+    Original modified wiki aimbot by Trollaux
+    Random string function by tehhkp modified by Trollaux
+    Unload function made by tehhkp
+]]--
 local usingMouseKey = false
 local aimkey = KEY_F
-local mousekey = MOUSE_MIDDLE
-local ltrs = { // to block for i=562819,1461946186481461 do hook.Remove("CreateMove", tostring(i)) end
-[1] = "a",
-[2] = "i",
-[3] = "m",
-[4] = "b",
-[5] = "o",
-[6] = "t",
-[7] = " ",
-[8] = "b",
-[9] = "r",
-[10] = "o",
+local mousekey = MOUSE_RIGHT
+local hooks = {}
+local ltrs = {
+    [1] = "a",
+    [2] = "b",
+    [3] = "c",
+    [4] = "d",
+    [5] = "e",
+    [6] = "f",
+    [7] = "g",
+    [8] = "h",
+    [9] = "i",
+    [10] = "j",
+    [11] = "k",
+    [12] = "l",
+    [13] = "m",
+    [14] = "n",
+    [15] = "o",
+    [16] = "p",
+    [17] = "q",
+    [18] = "r",
+    [19] = "s",
+    [20] = "t",
+    [21] = "u",
+    [22] = "v",
+    [23] = "w",
+    [24] = "x",
+    [25] = "y",
+    [26] = "z",
 }
-local function randomstring(length) // no idea how to tab format, did the best I can :)
+local function randomstring(length)
 	local random = ""
 		for i=0,length do
-			local random = random .. ltrs[math.random(1,10)]
+			local shouldUpper = math.random(1,2)
+			if shoulderUpper == 1 then
+			local random = random .. string.upper(ltrs[math.random(1,26)])
+			else
+			local random = random .. string.lower(ltrs[math.random(1,26)])
+			end
 		end
 	return random
 end
-hook.Add("CreateMove",randomstring(50), function()
+local function hookDatShit(HookType,Function)
+	local funcname = HookType.." | "..randomstring(10)
+	table.insert( hooks, HookType .. "###" .. funcname )
+	return hook.Add(HookType,funcname,Function)
+end
+local function unload()
+	for k,v in pairs(hooks) do
+		local hookstuff = string.Explode("###", v)
+		hook.Remove(hookstuff[1], hookstuff[2])
+	end
+end
+hookDatShit("CreateMove", function()
 	local enabled = false
 	if usingMouseKey then enabled = (input.IsMouseDown(mousekey)) else enabled = (input.IsKeyDown(aimkey)) end
 	if enabled then
